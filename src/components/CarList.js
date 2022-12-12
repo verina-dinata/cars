8d0194a5af56743f26692a63c3fd619672812c46
Verina Ong Dinata <verina.od1997@gmail.com>
2022-12-12 23:10:01 +0800

    add delete car feature

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,12 +1,15 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeCar } from '../store';
 
 function CarList() {
+  const dispatch = useDispatch();
+
   const cars = useSelector((state) => {
     return state.cars.data;
   });
 
   const handleCarDelete = (car) => {
-
+    dispatch(removeCar(car.id));
   };
 
   const renderedCars = cars.map((car) => {
