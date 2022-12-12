edaa2c9c35d1acacfe61fcee932e69f6e308fdf9
Verina Ong Dinata <verina.od1997@gmail.com>
2022-12-12 22:58:33 +0800

    rename carsSlice state from cars to data

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,4 +1,12 @@
+import { useSelector } from 'react-redux';
+
 function CarList() {
+  const cars = useSelector((state) => {
+    return state.cars.data;
+  });
+
+  console.log(cars);
+
   return(
     <div>CarList</div>
   );
