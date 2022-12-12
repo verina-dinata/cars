352b99e4849713f6d5e60cb270acc81a6ba0d071
Verina Ong Dinata <verina.od1997@gmail.com>
2022-12-12 23:38:18 +0800

    add auto cost calculate when user search for a car

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,6 +1,18 @@
+import { useSelector } from 'react-redux';
+
 function CarValue() {
+  const totalCost = useSelector(({ cars: { data, searchTerm } }) =>
+    data
+      .filter((car) =>
+       car.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      .reduce((acc, car) => acc + car.cost, 0)
+  );
+
   return(
-    <div>CarValue</div>
+    <div className="car-value">
+      Total Cost: ${totalCost}
+    </div>
   );
 }
 
