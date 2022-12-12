901f4533d45c52325021921d40c57cbb751ac405
Verina Ong Dinata <verina.od1997@gmail.com>
2022-12-12 22:21:53 +0800

    add index.js to export reducers

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { carsReducer, addCar, removeCar, changeSearchTerm } from "./slice/carsSlice";
+import { formReducer, changeName, changeCost } from "./slice/formSlice";
+
+const store = configureStore({
+  reducer: {
+    cars: carsReducer,
+    form: formReducer
+  }
+});
+
+export { store, changeName, changeCost, addCar, removeCar, changeSearchTerm };
